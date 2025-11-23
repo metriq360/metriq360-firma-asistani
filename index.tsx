2cd746dad5bd3a1eead74a458361d78533a01914
metriq360 <metriq360@gmail.com>
2025-11-23 20:16:00 +0300

    değişiklik

--- index.tsx
+++ index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+// React imports
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById('root');
+// Main App
+import App from "./App.tsx";
+
+// --- TÜM TSX DOSYALARINI BABEL'E DAHİL EDİYORUZ ---
+
+import "./DashboardViews.tsx";
+import "./AiAssistantView.tsx";
+import "./ProductAnalysisView.tsx";
+import "./AnalysisView.tsx";
+import "./DataEntryView.tsx";
+
+import "./LayoutComponents.tsx";
+
+// Services
+import "./firebaseService.ts";
+import "./geminiService.ts";
+
+// Constants
+import "./constants.ts";
+
+// --------------------------------------------------
+
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
